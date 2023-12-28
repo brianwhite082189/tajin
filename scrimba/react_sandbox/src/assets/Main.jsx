0b757fbe562e231afff5ec53f49e059a923d56ba
Brian White <brian.white082189@gmail.com>
2023-12-28 10:35:37 -0500

    main component added

---
+++ scrimba/react_sandbox/src/assets/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main () {
+    return(
+        <>
+            <ul>
+            <li>Was first released in 2013.</li>
+            <li>Was originally created by Jordan Walke.</li>
+            <li>Has well over 100K stars on GitHub.</li>
+            <li>Is Maintained by Facebook.</li>
+            <li>Powers thousands of enerprise apps, including mobil apps.</li>
+        </ul>
+      </>
+    )
+}
